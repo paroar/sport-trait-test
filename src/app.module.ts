89c40714accb08da180692a78083382094522b80
paroar <prodriguezarmida@gmail.com>
2023-01-10 13:24:42 +0100

    Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+  ],
+})
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors(CORS);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
+  console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
